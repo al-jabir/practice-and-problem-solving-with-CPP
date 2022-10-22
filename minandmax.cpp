40d9d03355bfd1ed0b939a2e5070550abb802f94
al-jabir <aljabir098@gmail.com>
2022-10-23 03:50:26 +0600

    min and max

---
+++ minandmax.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int a[5]={5,3,1,2,4};
+    int mn = ar[0];
+    int i;
+    for(i=1; i<5; i++){
+        mn=min(mn,ar[i]);
+
+        cout<<"index- "<<i"-> "<<mn<<endl;
+    }
+
+    return 0;
+}
